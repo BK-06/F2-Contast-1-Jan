f5d8957298945277b8bc69b0664b95877702ee52
Neon <105565447+BK-06@users.noreply.github.com>
2023-01-07 16:51:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,54 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
-function PrintDeveloperswithMap() {
+function PrintDeveloperswithMap(arr) {
   //Write your code here
+      arr.map(item=>{
+      if(item.profession==='developer'){
+          console.log(item.name); 
+      }
+  }) 
+  // if((arr[0].profession) === 'developer'){
+  //     console.log(arr[0].name);
+  // }
 }
+PrintDeveloperswithMap(arr);
 
-function PrintDeveloperbyForEach() {
+function PrintDeveloperbyForEach(arr) {
   //Write your code here
+      arr.forEach(item => {
+        if(item.profession==='developer'){
+            console.log(item.name);
+        }
+    });
 }
+PrintDeveloperbyForEach(arr);
 
-function addData() {
+function addData(arr) {
   //Write your code here
+    arr.push({id:4,name:"susan",age:"20",profession:"intern"}); 
+    console.log(arr);
 }
+addData(arr);
 
-function removeAdmin() {
+function removeAdmin(arr) {
   //Write your code here
+      for(let i=0;i<arr.length;i++){
+        if(arr[i].profession==='admin'){
+            arr.splice(i);
+        }
+    }
+    console.log(arr);
 }
+removeAdmin(arr)
 
-function concatenateArray() {
+function concatenateArray(arr) {
   //Write your code here
+      let arr2 = [
+        { id: 4, name: "kiran", age: "18", profession: "developer" },
+        { id: 5, name: "balu", age: "20", profession: "developer" },
+        { id: 6, name: "raj", age: "19", profession: "admin" },
+      ];
+      console.log(arr.concat(arr2));
 }
+  concatenateArray(arr)
